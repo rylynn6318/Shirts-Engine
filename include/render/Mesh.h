a4de360c07015b0125fcd7ee2f6f2fe642f0bd7a
donghyeon <gg6318@naver.com>
2021-02-07 18:00:26 +0900

    Mesh, Model 예제코드 추가 일단 추가만해둠 구조를 생각해야함

---
+++ include/render/Mesh.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "glad/glad.h"
+#include "glm/glm.hpp"
+#include "shader/Shader.h"
+
+namespace se
+{
+	struct Vertex
+	{
+		glm::vec3 Position;
+		glm::vec3 Normal;
+		glm::vec2 TexCoords;
+		glm::vec3 Tangent;
+		glm::vec3 Bitangent;
+	};
+
+	struct Texture 
+	{
+		unsigned int id;
+		std::string type;
+		std::string path;
+	};
+
+	class Mesh
+	{
+	public:
+		Mesh() = delete;
+		~Mesh();
+		Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+		auto draw(Shader shader)->void;
+
+	public:
+		std::vector<Vertex> vertices;
+		std::vector<unsigned int> indices;
+		std::vector<Texture> textures;
+
+	private:
+		GLuint VAO, VBO, EBO;
+		auto setUpMesh()->void;
+	};
+}
---
+++ include/render/Model.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "render/Mesh.h"
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+namespace se
+{
+	unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma);
+
+	class Model
+	{
+	public:
+		Model() = delete;
+		~Model();
+		Model(std::string const& path, bool gamma = false);
+		auto Draw(Shader& shader)->void;
+
+	public:
+		std::vector<Texture> textures_loaded;
+		std::vector<Mesh> meshes;
+		std::string directory;
+		bool gammaCorrection;
+
+	private:
+		auto loadModel(std::string const& path) -> void;
+		auto processNode(aiNode* node, const aiScene* scene) -> void;
+		auto processMesh(aiMesh* mesh, const aiScene* scene)->Mesh;
+		auto loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName)->std::vector<Texture>;
+	};
+
+}
