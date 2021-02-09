c5959647ced5cc03baf94c3e7b4926aaa6461dae
donghyeon <gg6318@naver.com>
2021-02-09 19:27:59 +0900

    인코딩 변경으로 인한 주석 수정

--- src/render/Texture.cpp
+++ src/render/Texture.cpp
@@ -38,7 +38,6 @@ bool se::Texture::loadTexture(const char* fileName)
 		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 
-		// �ؽ�ó wrapping/filtering �ɼ� ����
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
