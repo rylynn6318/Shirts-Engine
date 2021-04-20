19603c06b7a3bd0d08f3269e147a774cd79c1513
donghyeon <rylynn6318@kumoh.ac.kr>
2021-04-20 23:01:50 +0900

    배열 -> 벡터 구조 변경

--- include/utility/Dynamic_bitset.h
+++ include/utility/Dynamic_bitset.h
@@ -1,35 +1,40 @@
 #pragma once
+#pragma once
 #include <bitset>
+#include <vector>
 
 namespace se
 {
 	class Dynamic_bitset
 	{
 	public:
-		static constexpr size_t bit_size = 8;
+		static constexpr std::size_t bit_size = 8;
+		static constexpr std::size_t npos = SIZE_MAX;
 
-		using size_t = std::size_t;
 		using bitset = std::bitset<bit_size>;
-
+		 
 		Dynamic_bitset();
-		~Dynamic_bitset();
-		Dynamic_bitset(const Dynamic_bitset&) = delete;
-		Dynamic_bitset& operator=(const Dynamic_bitset&) = delete;
+		~Dynamic_bitset() = default;
+		Dynamic_bitset(const Dynamic_bitset&) = default;
+		Dynamic_bitset& operator=(const Dynamic_bitset&) = default;
+		bool operator==(const Dynamic_bitset&) const;
 
-		void set(size_t pos);
-		bool get(size_t pos) const;
-		size_t size() const noexcept;
+		void set(std::size_t pos);
+		void reset(std::size_t pos);
+		bool get(std::size_t pos) const;
+		std::size_t size() const noexcept;
 		void clear();
-		void resize(size_t size);
+		void resize(std::size_t size);
 		bool empty() const noexcept;
 		std::string to_string() const noexcept;
 
 		bool is_proper_subset_of(const Dynamic_bitset& bitset) const;
+		std::size_t find_first() const noexcept;
+		std::size_t find_next(std::size_t pos) const noexcept;
 
 	private:
-		size_t numbits;
-
-		bitset _bitset;
-		bitset* bitPtr;
+		std::size_t numbits;
+		std::vector<bitset> bitArray;
+		std::size_t find_from(std::size_t pos) const noexcept;
 	};
 }
--- src/utility/Dynamic_bitset.cpp
+++ src/utility/Dynamic_bitset.cpp
@@ -1,57 +1,161 @@
 #include "utility/Dynamic_bitset.h"
 #include <algorithm>
 #include <string>
+#include <iostream>
 
-se::Dynamic_bitset::Dynamic_bitset() : _bitset(0), numbits(se::Dynamic_bitset::bit_size)
+se::Dynamic_bitset::Dynamic_bitset() :
+	numbits(0),
+	bitArray{bitset()}
 {
-	bitPtr = new bitset[bit_size];
+	
 }
 
-se::Dynamic_bitset::~Dynamic_bitset()
-{
-	delete[] bitPtr;
-	bitPtr = nullptr;
-}
-
-size_t se::Dynamic_bitset::size() const noexcept
+std::size_t se::Dynamic_bitset::size() const noexcept
 {
 	return numbits;
 }
 
-void se::Dynamic_bitset::clear()
+bool se::Dynamic_bitset::operator==(const Dynamic_bitset& other) const
 {
-	bitPtr->reset();
-}
+	auto& big = (bitArray.size() <= other.bitArray.size()) ? other.bitArray : bitArray;
+	auto& small = (bitArray.size() > other.bitArray.size()) ? other.bitArray : bitArray;
 
-void se::Dynamic_bitset::resize(size_t size)
-{
-	if (numbits <= size)
+	for (int i = small.size(); i < big.size(); i++)
 	{
-		size_t block_size = size / bit_size;
-		numbits = bit_size * (block_size + 1);
-		auto newPtr = new bitset[block_size + 1];
-		std::copy(bitPtr, bitPtr + block_size + 1, newPtr);
-		delete[] bitPtr;
+		if (big[i].any())
+			return false;
+	}
 
-		bitPtr = newPtr;
+	if (std::equal(small.cbegin(), small.cend(), big.cbegin()))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
 	}
 }
 
-void se::Dynamic_bitset::set(size_t pos)
+void se::Dynamic_bitset::clear()
+{
+
+}
+
+void se::Dynamic_bitset::resize(std::size_t size)
+{
+	if (numbits <= size)
+	{
+		std::size_t block_size = size / bit_size;
+		numbits = bit_size * (block_size + 1);
+		for (int i = 0; i < block_size; i++)
+		{
+			bitArray.push_back(bitset());
+		}
+	}
+}
+
+void se::Dynamic_bitset::set(std::size_t pos)
 {
 	if (numbits <= pos)
 	{
 		resize(pos);
 	}
-	auto& bits = bitPtr[pos / bit_size];
-	bits[pos % bit_size] = pos;
+	auto& bits = bitArray[pos / bit_size];
+	bits[pos % bit_size] = true;
 }
 
-bool se::Dynamic_bitset::get(size_t pos) const
+void se::Dynamic_bitset::reset(std::size_t pos)
+{
+	auto& bits = bitArray[pos / bit_size];
+	bits[pos % bit_size] = false;
+}
+
+bool se::Dynamic_bitset::get(std::size_t pos) const
 {
 	if (numbits <= pos)
 		return false;
 
-	auto& bits = bitPtr[pos / bit_size];
+	auto& bits = bitArray[pos / bit_size];
 	return bits[pos % bit_size];
+}
+
+std::string se::Dynamic_bitset::to_string() const noexcept
+{
+	std::string s;
+	for (int i = numbits / bit_size - 1; i > -1; i--)
+	{
+		s += bitArray[i].to_string() + " ";
+	}
+
+	return s;
+}
+
+bool se::Dynamic_bitset::is_proper_subset_of(const Dynamic_bitset& other) const
+{
+	if (bitArray.size() == other.bitArray.size() && std::equal(bitArray.cbegin(), bitArray.cend(), other.bitArray.cbegin()))
+	{
+		return false;
+	}
+
+	if (bitArray.size() <= other.bitArray.size())
+	{
+		for (int i = 0; i < bitArray.size(); i++)
+		{
+			if ((bitArray[i] & other.bitArray[i]) != bitArray[i])
+			{
+				return false;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < other.bitArray.size(); i++)
+		{
+			if ((bitArray[i] & other.bitArray[i]) != bitArray[i])
+			{
+				return false;
+			}
+		}
+		for (int i = bitArray.size(); i < bitArray.size(); i++)
+		{
+			if (bitArray[i].any())
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+std::size_t se::Dynamic_bitset::find_first() const noexcept
+{
+	return find_from(0);
+}
+
+std::size_t se::Dynamic_bitset::find_next(std::size_t pos) const noexcept
+{
+	return find_from(pos + 1);
+}
+
+std::size_t se::Dynamic_bitset::find_from(std::size_t pos) const noexcept
+{
+	bool flag = true;
+	for (int i = pos / bit_size; i < bitArray.size(); i++)
+	{
+		for (int j = 0; j < bitArray[i].size(); j++)
+		{
+			if (i == 0 && flag)
+			{
+				j = pos;
+				flag = false;
+			}
+			if (bitArray[i][j])
+			{
+				return i * bit_size + j;
+			}
+		}
+	}
+
+	return npos;
 }
